2b7520f86fdd7ad6efdd3854f2115687db575199
Liyuan Li <liyuanl@codeaurora.org>
2011-11-11 13:47:59 -0800

    video: msm: Adding support for MDP4 HSIC controls
    Adding support for users to adjust Hue, Saturation, Intensity and
    Contrast of the display.
    
    Change-Id: I79cb69c871686ccba115798f635f768b9a50affc
    Signed-off-by: Liyuan Li <liyuanl@codeaurora.org>
    Signed-off-by: Ronny Schmatzler <schmatzler@googlemail.com>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2116,6 +2116,10 @@ int mdp4_overlay_set(struct fb_info *info, struct mdp_overlay *req)
 		}
 	}
 
+	/* precompute HSIC matrices */
+	if (req->flags & MDP_DPP_HSIC)
+		mdp4_hsic_set(pipe, &(req->dpp));
+
 	mdp4_stat.overlay_set[pipe->mixer_num]++;
 
 	if (ctrl->panel_mode & MDP4_PANEL_MDDI) {
@@ -2167,6 +2171,8 @@ int mdp4_overlay_unset(struct fb_info *info, int ndx)
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
 	struct mdp4_overlay_pipe *pipe;
 	uint32 flags;
+	struct dpp_ctrl dpp;
+	int i;
 
 	if (mfd == NULL)
 		return -ENODEV;
@@ -2253,6 +2259,15 @@ int mdp4_overlay_unset(struct fb_info *info, int ndx)
 		}
 	}
 #endif
+
+	/* Reset any HSIC settings to default */
+	if (pipe->flags & MDP_DPP_HSIC) {
+		for (i = 0; i < NUM_HSIC_PARAM; i++)
+			dpp.hsic_params[i] = 0;
+
+		mdp4_hsic_set(pipe, &dpp);
+		mdp4_hsic_update(pipe);
+	}
 
 	mdp4_stat.overlay_unset[pipe->mixer_num]++;
 
@@ -2544,6 +2559,10 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 #endif
 		}
 	}
+
+	/* write out DPP HSIC registers */
+	if (pipe->flags & MDP_DPP_HSIC)
+		mdp4_hsic_update(pipe);
 
 	mdp4_stat.overlay_play[pipe->mixer_num]++;
 	mutex_unlock(&mfd->dma->ov_mutex);
