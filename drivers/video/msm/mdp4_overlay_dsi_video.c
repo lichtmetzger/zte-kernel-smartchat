freedreno-zz/kernel-msm

msm_fb: display: Configure DMA before Layer Mixer for writeback mode switch
DMA addresses are configured after Layer Mixer is set to writeback mode.
This leads to IOMMU page faults due to incorrect addresses read from DMA.
Hence DMA is programmed before Mixer setting.

Change-Id: I4c5e41a56f05e3fabb46b4cadde03168589eeb17
Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
(cherry picked from commit bba0d71bedb598f8f8dce9cb4d04e974b30775fc)

Signed-off-by: Dhivya Subramanian <dthiru@codeaurora.org>

drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -658,8 +658,13 @@ void mdp4_dma_p_done_dsi_video(struct mdp_dma_data *dma)
 {
 	if (blt_cfg_changed) {
 		mdp_is_in_isr = TRUE;
-		mdp4_overlayproc_cfg(dsi_pipe);
-		mdp4_overlay_dmap_xy(dsi_pipe);
+		if (dsi_pipe->blt_addr) {
+			mdp4_overlay_dmap_xy(dsi_pipe);
+			mdp4_overlayproc_cfg(dsi_pipe);
+		} else {
+			mdp4_overlayproc_cfg(dsi_pipe);
+			mdp4_overlay_dmap_xy(dsi_pipe);
+		}
 		mdp_is_in_isr = FALSE;
 		if (dsi_pipe->blt_addr) {
 			mdp4_dsi_video_blt_ov_update(dsi_pipe);

lichtmetzger/zte-kernel-smartchat

Signed-off-by: Ronny Schmatzler <schmatzler@googlemail.com>

drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -570,8 +570,13 @@ void mdp4_dma_p_done_dsi_video(struct mdp_dma_data *dma)
 {
 	if (blt_cfg_changed) {
 		mdp_is_in_isr = TRUE;
-		mdp4_overlayproc_cfg(dsi_pipe);
-		mdp4_overlay_dmap_xy(dsi_pipe);
+		if (dsi_pipe->ov_blt_addr) {
+			mdp4_overlay_dmap_xy(dsi_pipe);
+			mdp4_overlayproc_cfg(dsi_pipe);
+		} else {
+			mdp4_overlayproc_cfg(dsi_pipe);
+			mdp4_overlay_dmap_xy(dsi_pipe);
+		}
 		mdp_is_in_isr = FALSE;
 		if (dsi_pipe->ov_blt_addr) {
 			mdp4_dsi_video_blt_ov_update(dsi_pipe);