da564ddce95ee0d4136372a13c6e9ae1d385806f
Ronny Schmatzler <schmatzler@googlemail.com>
2015-06-28 19:08:11 +0200

    Adjust memory sizes

--- arch/arm/mach-msm/board-zte-turies.c
+++ arch/arm/mach-msm/board-zte-turies.c
@@ -1540,27 +1540,39 @@ static struct msm_i2c_platform_data msm_gsbi1_qup_i2c_pdata = {
 };
 
 #ifdef CONFIG_ARCH_MSM7X27A
+/* old values
 //ruanmeisi for liuhaitao 20111220, HVGA(480x320)
-//#define MSM_PMEM_MDP_SIZE       0x1DD1000 //stock p752d
-//#define MSM_PMEM_MDP_SIZE       (13*1024*1024) //codeaurora spec, 13M
-#define MSM_PMEM_MDP_SIZE       0x1400000	// 20M
+#define MSM_PMEM_MDP_SIZE       0x1DD1000 //stock p752d
+#define MSM_PMEM_MDP_SIZE       (13*1024*1024) //codeaurora spec, 13M
 #define MSM7x25A_MSM_PMEM_MDP_SIZE	0x1000000
-//#define MSM_PMEM_ADSP_SIZE      0x1000000 //stock p752d
-//#define MSM_PMEM_ADSP_SIZE      0xB71000 //codeaurora spec, 11.5M
-#define MSM_PMEM_ADSP_SIZE      0xC00000	// 12M
+#define MSM_PMEM_ADSP_SIZE      0x1000000 //stock p752d
+#define MSM_PMEM_ADSP_SIZE      0xB71000 //codeaurora spec, 11.5M
 #define MSM7x25A_MSM_PMEM_ADSP_SIZE      0xB91000
 
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 //ruanmeisi for liuhaitao 20111220, HVGA(480x320)
-//#define MSM_FB_SIZE		0x260000 //stock p752d
-//#define MSM_FB_SIZE		(1*1024*1024) //codeaurora spec, 1M
-#define MSM_FB_SIZE		0x500000 //5M
+#define MSM_FB_SIZE		0x260000 //stock p752d
+#define MSM_FB_SIZE		(1*1024*1024) //codeaurora spec, 1M
+#else
+#define MSM_FB_SIZE		0x195000 //stock*/
+
+//values from Konstat (atlas40)
+#define MSM_PMEM_MDP_SIZE       0x2300000
+#define MSM7x25A_MSM_PMEM_MDP_SIZE       0x1500000
+
+#define MSM_PMEM_ADSP_SIZE      0x1000000
+#define MSM7x25A_MSM_PMEM_ADSP_SIZE      0xB91000
+
+
+#ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
+#define MSM_FB_SIZE		0x46512C
 #define MSM7x25A_MSM_FB_SIZE	0xE1000
 #else
-//#define MSM_FB_SIZE		0x195000 //stock
 #define MSM_FB_SIZE		0x196000
-#define MSM7x25A_MSM_FB_SIZE	0xE1000
+#define MSM7x25A_MSM_FB_SIZE	0x96000
+
+
 #endif
 
 #endif
